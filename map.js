68b6f08bc22cdc2eefc6916771b46f8ade8f0972
Noctambulique <monsieurpyjama@gmail.com>
2025-08-25 17:21:57 +0200

    Create map.js

---
+++ map.js
@@ -0,0 +1,7 @@
+// Définition de la carte (ville)
+const town = {
+  houses: [
+    { id: "house1", x: 300, y: 200, w: 80, h: 80, door: { x: 330, y: 260, w: 20, h: 20 } },
+    { id: "house2", x: 500, y: 350, w: 100, h: 100, door: { x: 540, y: 420, w: 24, h: 22 } }
+  ]
+};

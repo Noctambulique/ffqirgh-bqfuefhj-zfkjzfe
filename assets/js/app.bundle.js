afd40d70f35dbedcec6fc512898d73c059a5b718
Noctambulique <monsieurpyjama@gmail.com>
2025-09-21 17:37:59 +0200

    Update app.bundle.js

--- assets/js/app.bundle.js
+++ assets/js/app.bundle.js
@@ -37,7 +37,7 @@
     'House/20250909_1935_Centre Culturel Nocturne_simple_compose_01k4qrpxm6epeadq2gexry06a0-Photoroom.png',
     'House/20250909_1938_Centre des Lubies Nocturne_simple_compose_01k4qrtxbwf2v8jnw0wex32px9-Photoroom.png',
     'House/Immeuble_Bibliotheque-Photoroom.png',
-    'House20250821_1840_Musée Éclairé la Nuit_simple_compose_01k36qyzzbfcq90cqn3vpsgaj9-Photoroom.png',
+    'House/House20250821_1840_Musée Éclairé la Nuit_simple_compose_01k36qyzzbfcq90cqn3vpsgaj9-Photoroom.png',
     "House/20250822_1305_Centre d'archives nocturne_simple_compose_01k38q6wp3endsa7d41y0w1wke-Photoroom.png",
     'House/20250909_1948_Bibliothèque Éclairée Nocturne_simple_compose_01k4qsdqwmej0vaswh1fnbzmt8 (1)-Photoroom.png',
   ];
@@ -517,6 +517,7 @@
     }
   });
 })();
+
 
 
 

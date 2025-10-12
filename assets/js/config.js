ff82a3d71dd01df326a08b0eda73b6378995aac6
Noctambulique <monsieurpyjama@gmail.com>
2025-10-12 22:34:45 +0200

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -17,18 +17,16 @@
       1: 'ampleFolder/Film Art Jeu.pdf',
       2: 'ampleFolder/Lubies.pdf',
       3: 'ampleFolder/Power Point Ultime +.pdf',
-      4: 'ampleFolder/Dictionnaire.pdf',
-      5: 'map2.html',
-      6: 'map3.html',
+      4: 'map2.html',
+      5: 'map3.html',
     },
   },
   names: {
     1: 'Médiathèques',
     2: 'Centre des Lubies',
     3: 'Bureau du Détective',
-    4: 'Musée des Songes',
-    5: 'QG de Philosophie',
-    6: 'Hotel des Libraires',
+    4: 'QG de Philosophie',
+    5: 'Hotel des Libraires',
   },
 });
 
@@ -36,12 +34,12 @@ export const HOUSE_IMAGES = [
   'House/Médiathèque.png',
   'House/CentredesLubies.png',
   'House/BureauDuDetective.png',
-  'House/MuséeDesSonges.png',
   'House/QGPhilosophe.png',
   "House/HoteldesLibraires.png",
 ];
 
 export const BACKGROUND_SRC = 'http://www.image-heberg.fr/files/17570213431743313302.png';
+
 
 
 
